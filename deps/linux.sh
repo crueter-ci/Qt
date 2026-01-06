56f2c1ec0b4d3d160b9bae233c72270e81d85522
crueter <crueter@eden-emu.dev>
2026-01-06 14:23:24 -0500

    fix linux deps
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- deps/linux.sh
+++ deps/linux.sh
@@ -7,4 +7,10 @@ sudo apt-get install -y --no-install-recommends \
 	libxrender-dev libxkbcommon-dev libxkbcommon-x11-dev zstd \
 	libssl-dev zlib1g-dev libfreetype6-dev libpng-dev libjpeg-dev \
 	libasound2-dev libpulse-dev libdbus-1-dev libfontconfig1-dev \
+	libgtk-3-dev \
+	libxext-dev libxfixes-dev libxi-dev \
+	libxcb-cursor-dev libxcb-glx0-dev libxcb-icccm4-dev \
+	libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev \
+	libxcb-render-util0-dev libxcb-shape0-dev libxcb-shm0-dev \
+	libxcb-sync-dev libxcb-util-dev libxcb-xfixes0-dev libxcb-xkb-dev \
 	tar xz-utils ccache
