0f8fb03e0bab14c58e6a130efdd391d20f273c9a
crueter <crueter@eden-emu.dev>
2026-01-23 23:58:15 -0500

    what? (#13)
    * what?
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    * openssl static libs?
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    * fix openssl once and for all, hopefully
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    ---------
    
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -107,7 +107,8 @@ configure() {
 
 		export PKG_CONFIG_PATH="$OPENSSL_DIR/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-		set -- "$@" -DOPENSSL_USE_STATIC_LIBS=ON -DFFMPEG_DIR="$FFMPEG_DIR" -DOPENSSL_ROOT_DIR="$OPENSSL_DIR" -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" -DCMAKE_PREFIX_PATH="$OPENSSL_DIR" -DOpenSSL_ROOT="$OPENSSL_DIR"
+		set -- "$@" -DOPENSSL_USE_STATIC_LIBS=ON -DFFMPEG_DIR="$FFMPEG_DIR" -DOPENSSL_ROOT_DIR="$OPENSSL_DIR" \
+			-DCMAKE_FIND_LIBRARY_SUFFIXES=".a" -DCMAKE_PREFIX_PATH="$OPENSSL_DIR" -DOpenSSL_ROOT="$OPENSSL_DIR" # -DOPENSSL_USE_STATIC_LIBS=ON
 
 		echo "-- * FFmpeg dir: $FFMPEG_DIR"
 		echo "-- * OpenSSL dir: $OPENSSL_DIR"
@@ -205,6 +206,7 @@ configure() {
 		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"
 
 	grep -i 'libssl' CMakeCache.txt || true
+	grep -i 'libcrypto' CMakeCache.txt || true
 }
 
 build() {
