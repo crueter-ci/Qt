782833c34181f13775153e9a1e4aaad2d01e5095
crueter <crueter@eden-emu.dev>
2026-01-23 20:29:04 -0500

    u
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- deps/arch.sh
+++ deps/arch.sh
@@ -2,7 +2,7 @@
 
 pacman -Syu --needed --noconfirm \
 	base-devel cmake ninja git unzip python \
-	vulkan-headers vulkan-loader mesa libglvnd glu \
+	vulkan-headers vulkan-icd-loader mesa libglvnd glu \
 	pkgconf libxrandr libxkbcommon-x11 zstd xcb-util-cursor \
 	gtk3 dbus alsa-lib libpulse fontconfig libpng libjpeg-turbo \
 	zlib xcb-util tar xz ccache
