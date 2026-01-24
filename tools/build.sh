60f9e8411486e798d8a42928fbc4346675412b20
crueter <crueter@eden-emu.dev>
2026-01-23 23:12:53 -0500

    what?
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -202,9 +202,10 @@ configure() {
 		-no-feature-quickcontrols2-fluentwinui3 -no-feature-testlib -no-feature-qml-preview -no-feature-qml-profiler \
 		-- "$@" \
 		-DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET}" \
-		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"
+		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" --debug-find-pkg=OpenSSL
 
 	grep -i 'libssl' CMakeCache.txt || true
+	grep -i 'libcrypto' CMakeCache.txt || true
 }
 
 build() {
