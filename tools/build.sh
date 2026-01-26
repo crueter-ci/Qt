df46ef1e70fa4d9108448fba28bd7c47dba02ec3
crueter <crueter@eden-emu.dev>
2026-01-25 21:10:52 -0800

    windows: disable winrt (#15)
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -203,7 +203,7 @@ configure() {
 		-no-feature-quickcontrols2-fluentwinui3 -no-feature-testlib -no-feature-qml-preview -no-feature-qml-profiler \
 		-- "$@" \
 		-DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET}" \
-		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"
+		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" -DQT_FEATURE_cpp_winrt=OFF
 
 	grep -i 'libssl' CMakeCache.txt || true
 	grep -i 'libcrypto' CMakeCache.txt || true
