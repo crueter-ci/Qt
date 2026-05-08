95241c5c86922c598c76e6d2484dbbe63a656a61
crueter <crueter@eden-emu.dev>
2026-05-08 17:28:18 -0400

    6.11
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/vars.sh
+++ tools/vars.sh
@@ -9,7 +9,7 @@ win7_repo="qt6windows7"
 
 case "$VERSION" in
 	# no qt6windows7 builds yet.
-	6.10.2|6.7.3) ;;
+	6.11.0|6.10.2|6.7.3) ;;
 	# Qt6Windows7 stuff
 	6.9.3)
 		win7_commit="78074b12a94b56bb2b929f243fb9351b3f9e2439"
